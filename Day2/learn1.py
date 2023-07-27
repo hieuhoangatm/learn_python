c1dac300bdf40f82ca5705d343cc83b9ce1be8a7
hieuhoangatm <hieuhoangatm@gmail.com>
2023-07-27 22:09:16 +0700

    new

--- Day2/learn1.py
+++ Day2/learn1.py
@@ -7,22 +7,22 @@ list slicing
 
 """
 
-friend = [["hieu",20], ["hung",14], ["huy",18]]
+friend = [["hieu", 20], ["hung", 14], ["huy", 18]]
 
 print(type(friend[0]))
 
 print(friend[0][0])
 
-list1 = [1,3,4]
+list1 = [1, 3, 4]
 list2 = list1.copy()
 print(list1 is list2)
 
 list3 = list1
 print(list3 is list1)
 
-ds = [1,3,4,56,45]
+ds = [1, 3, 4, 56, 45]
 
-new_ds= ds[0:3:1]
+new_ds = ds[0:3:1]
 
 print(new_ds)
 
